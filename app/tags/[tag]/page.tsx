daee83a0e3e7e73284b2bf07c99016e64fb26e9b
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-07-26 14:10:44 +0200

    add dynamic tags

---
+++ app/tags/[tag]/page.tsx
@@ -0,0 +1,65 @@
+import { getPostsMeta } from "@/lib/posts";
+import ListItem from "@/app/components/ListItem";
+import Link from "next/link";
+
+
+export const revalidate = 0
+
+type Props = {
+    params: {
+        tag: string
+    }
+}
+
+/* 
+export async function generateStaticParams() {
+    const posts = await getPostsMeta()
+
+    if (!posts) return []
+
+    const tags = new Set(posts.map((post) => post.tags).flat())
+
+    return Array.from(tags).map((tag) => ({tag}))
+}
+*/
+
+
+export function generateMetadata({params: { tag }}: Props) {
+    return { title: `Posts about ${tag}`}
+}
+
+
+
+
+export async function TagPostList({params: { tag }}: Props) {
+    const posts = await getPostsMeta()
+
+    if (!posts) return <p>Sorry, no posts available..</p>
+
+    const tagPosts = posts.filter(post => post.tags.includes(tag))
+
+    if (!tagPosts.length) {
+        return (
+            <div>
+                <p>Sorry, no posts for  that keyword..</p>
+                <Link href='/' >Back home</Link>
+            </div>
+        )
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
